b0e64d7dd73f72098293299b988f73692265d559
Matúš Revický <matus.revicky@student.upjs.sk>
2020-11-29 19:38:14 +0100

    example routes removed

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,5 @@
-import examplesRouter from './api/controllers/examples/router';
 import zabbixRouter from './api/controllers/examples/zabbixrouter';
 
 export default function routes(app) {
-  app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/zabbix', zabbixRouter);
 }
